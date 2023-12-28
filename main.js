b6f9d61137c7a69d2cab3f7d738d5fd042191658
BarryTheGreat <bharathshivaji2005@gmail.com>
2023-12-28 23:52:21 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+const btnHeight = 110;
+const btnWidth = 160;
+
+const maxWidth = window.innerWidth - btnWidth;
+const maxHeight = window.innerHeight - btnHeight;
+
+window.addEventListener('DOMContentLoaded',()=>{
+const btn = document.getElementById('moving');
+
+btn.addEventListener('click',()=>alert('Hey there'));
+
+btn.addEventListener('mouseover',()=>{
+  btn.style.left = Math.floor(Math.random()*(maxWidth+1)) + 'px';
+  btn.style.top = Math.floor(Math.random()* (maxHeight +1)) + 'px'
+})
+})
